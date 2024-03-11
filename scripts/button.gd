f2d169a7c7855088728d71f92c232083b3cd2b72
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-03-11 23:51:17 +0400

    Add moving background to loading screen

--- scripts/button.gd
+++ scripts/button.gd
@@ -5,6 +5,5 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	animation_player.play("Click")
 
-
 func _on_body_exited(body: Node3D) -> void:
 	animation_player.play_backwards("Click")
